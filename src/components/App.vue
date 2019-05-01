678cc751ef234991217e97abcb094b301239b4e1
ti.fabio <ti.fabio@gmail.com>
2019-05-01 15:38:50 -0400

    Estrutura inicial

---
+++ src/components/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div class="container mt-3">
+      <div class="input-group">
+        <input type="text" class="form-control">
+        <span class="input-group-btn">
+          <button class="btn btn-info" type="button">Add</button>
+        </span>
+      </div>
+      <hr>
+      <ul class="list-group">
+        <li class="list-group-item" style="cursor:pointer">Tarefa 1
+          <button class="btn btn-sm btn-danger float-right">Del</button>
+        </li>
+        <li class="list-group-item">Tarefa 2
+          <button class="btn btn-sm btn-danger float-right">Del</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
